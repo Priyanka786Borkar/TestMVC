8645de23fbee137386f79a2c8d6a387bb7447b7a
Priyanka786Borkar <priyankaborkar.786@gmail.com>
2022-11-06 15:13:55 +0530

    Add project files.

---
+++ TestMVC/Controllers/TestMVC/TestMVCController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC.Models;
+
+namespace TestMVC.Controllers.TestMVC
+{
+    public class TestMVCController : Controller
+    {
+        // GET: TestMVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SaveTestMVC (TestMVCModel model)
+        {
+            try
+            {
+                return Json (new {msg = (new )})
+            }
+        }
+    }
+}
---
+++ TestMVC/Models/TestMVC/TestMVCModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestMVC.Data;
+
+namespace TestMVC.Models.TestMVC
+{
+    public class TestMVCModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+        public string SaveTestMVC(TestMVCModel model)
+        {
+            string msg = "";
+            TestMVCEntities db = new TestMVCEntities();
+            var SaveTestMVC = new tblTest()
+            {
+                Name = model.Name,
+                Age = model.Age,
+            };
+            db.tblTests.Add(SaveTestMVC);
+            db.SaveChanges();
+            return msg;
+        }
+    }
+    
+}
